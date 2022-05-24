8d9d3011160d9d8cbc88df1e8f6659692795fc4e
malomiquel <malomiquel@gmail.com>
2022-05-24 14:30:46 +0200

    Exercice 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,31 @@ import { Statistique } from './models/statistique';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  statistique1: Statistique = {
-    identifiant: 'stat1',
-    titre: 'Statistique 1',
-    valeur: '20M'
+  tabStatistique: Statistique[] = [
+    {
+      identifiant: 'stat1',
+      titre: 'Statistique 1',
+      valeur: '20M',
+    },
+    {
+      identifiant: 'stat2',
+      titre: 'Statistique 2',
+      valeur: '50M',
+    },
+  ];
+
+  nouvelleStatistic: Statistique = {
+    identifiant: 'stat3',
+    titre: 'Statistique 3',
+    valeur: '100M',
   };
 
-  statistique2: Statistique = {
-    identifiant: 'stat2',
-    titre: 'Statistique 2',
-    valeur: '50M'
-  };
+  constructor() {
+    setTimeout(() => {
+      this.tabStatistique.push(this.nouvelleStatistic);
+    }, 3000);
+  }
 }
