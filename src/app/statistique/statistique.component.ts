f20e90feeebc321cd7cc505c6c7198239781541d
malomiquel <malomiquel@gmail.com>
2022-05-24 14:22:09 +0200

    Exercice 5

---
+++ src/app/statistique/statistique.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Statistique } from '../models/statistique';
+
+@Component({
+  selector: 'app-statistique',
+  templateUrl: './statistique.component.html',
+  styleUrls: ['./statistique.component.css']
+})
+export class StatistiqueComponent implements OnInit {
+  @Input() statistique: Statistique | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
